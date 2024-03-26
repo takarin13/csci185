2f06ddf423ff7ce849ac10d2e98587c43b3230bf
takarin13 <rtakagi@unca.edu>
2024-03-25 22:19:38 -0400

    add to my homepage

---
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -0,0 +1,15 @@
+function makeRed(){
+    document.querySelector("body").style.backgroundColor = "#B23";
+}
+
+function makeBlue(){
+    document.querySelector("body").style.backgroundColor = "#2af";
+}
+
+function makeGreen(){
+    document.querySelector("body").style.backgroundColor = "#0f0";
+}
+
+//function changeColor(color){
+    //document.querySelecttor("body").style.backgroundColor = color
+//}
